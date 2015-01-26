26fec7850eea49a1bd9f06d40458cf136f0451ae
Mohamed Hamid <Hamid_ccp@hotmail.com>
2015-01-26 23:31:18 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset=utf-8 />
+	<title>IEEE AlexSB | Tweets</title>
+	<script src="/IEEE/includes/jquery-latest.min.js"></script>
+	<!--[if IE]>
+	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="/IEEE/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link href="/IEEE/bootstrap/css/custom.css" rel="stylesheet">
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<h2>#IEEEALexSB</h2>
+			<div class="col-md-6 col-md-offset-3">
+				<a class="twitter-timeline"
+				href="https://twitter.com/hashtag/IEEEAlexSB" 
+				data-widget-id="559804075911688192">#IEEEAlexSB Tweets</a>
+				</div>
+		</div>
+	</div>
+		<script>
+			!function(d,s,id){
+				var js,fjs=d.getElementsByTagName(s)[0],
+				p=/^http:/.test(d.location)?'http':'https';
+				if(!d.getElementById(id)){
+					js=d.createElement(s);
+					js.id=id;js.src=p+"://platform.twitter.com/widgets.js";
+					fjs.parentNode.insertBefore(js,fjs);
+				}
+			}(document,"script","twitter-wjs");
+			</script>
+
+		<script src="/IEEE/bootstrap/js/bootstrap.min.js"></script>
+	</body>
+</html>
